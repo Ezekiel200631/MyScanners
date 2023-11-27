fd90d715d5ab68ccfb79d920f30497383fc75a01
Ezekiel Lopez <ezekiel.lopez@mrjaffesclass.com>
2023-11-27 11:35:18 -0800

    Commit message

---
+++ MyScanner/ScannersTimesThree.java
@@ -0,0 +1,19 @@
+
+public class ScannersTimesThree
+{
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) { 
+            if (scanner.hasNext()) { 
+                String str = scanner.next(); 
+                System.out.println(str); 
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+}
