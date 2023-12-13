f34fab78211a22402ade59da49dacaa87ac222bc
Ezekiel Lopez <ezekiel.lopez@mrjaffesclass.com>
2023-12-13 11:25:12 -0800

    Commit message

--- MyScanner/scanningSpaces.java
+++ MyScanner/scanningSpaces.java
@@ -1,33 +1,20 @@
+import java.util.Scanner;
 
-/**
- * Write a description of class scanningSpaces here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class scanningSpaces
 {
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class scanningSpaces
-     */
-    public scanningSpaces()
-    {
-        // initialise instance variables
-        x = 0;
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) { 
+            if (scanner.hasNext()) { 
+                String str = scanner.next(); 
+                System.out.println(str); 
+            }
+        }
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String[] args) {
+        scanningSpaces scanningSpaces = new scanningSpaces();
+        scanningSpaces.run();
     }
 }
