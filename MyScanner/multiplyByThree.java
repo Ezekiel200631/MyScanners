d76b56c9fcaeac73bf8509368d2c953fd40d1508
Ezekiel Lopez <ezekiel.lopez@mrjaffesclass.com>
2023-12-14 11:03:00 -0800

    Commit message

--- MyScanner/multiplyByThree.java
+++ MyScanner/multiplyByThree.java
@@ -5,7 +5,6 @@ public class multiplyByThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
         for (int i = 0; i < 5; i++) {
             if (scanner.hasNextInt()) {
                 int number = scanner.nextInt();
